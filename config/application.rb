811e6eac13ef7b7c4e220af0277f7c664fb5f2e4
Pete Nicholls <pete@metanation.com>
2017-03-16 20:03:10 +1300

    Extract large constant to configuration file

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,6 @@ module BeautyIsEverywhere
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.quotes = Rails.root.join('config', 'quotes.txt').read.split("\n")
   end
 end
